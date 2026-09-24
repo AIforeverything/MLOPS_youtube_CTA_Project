6103d462d0a1a4477d2533dc4c9852953570ae49
Sudhakar <om.sudhakarrecruit@gmail.com>
2026-09-24 08:26:55 +0530

    dvc is in progress: model prediction is completed.

--- src/model/predict_model.py
+++ src/model/predict_model.py
@@ -18,20 +18,22 @@ logger = configure_logger()
 load_dotenv()
 dagshub_token = os.getenv("DAGSHUB_TOKEN")
 dagshub_url = "https://dagshub.com"
-repo_owner = os.getenv('repo_owner')
-repo_name = os.getenv('repo_name')
+repo_owner = os.getenv("repo_owner")
+repo_name = os.getenv("repo_name")
 if not dagshub_token:
-    raise EnvironmentError(
-        "dagshub_token  not loaded properly.")
+    raise EnvironmentError("dagshub_token  not loaded properly.")
 if not dagshub_url:
-    raise EnvironmentError(
-        "dagshub_url  not loaded properly.")
+    raise EnvironmentError("dagshub_url  not loaded properly.")
 if not repo_owner:
-    raise EnvironmentError(
-        "repo_owner  not loaded properly.")
+    raise EnvironmentError("repo_owner  not loaded properly.")
 if not repo_name:
-    raise EnvironmentError(
-        "repo_name  not loaded properly.")
+    raise EnvironmentError("repo_name  not loaded properly.")
+
+mlflow.set_tracking_uri(f"{dagshub_url}/{repo_owner}/{repo_name}.mlflow")
+logger.info(f"MLflow Tracking URI: {mlflow.get_tracking_uri()}")
+
+os.environ["MLFLOW_TRACKING_USERNAME"] = repo_owner
+os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
 
 
 def model_predict(test_data_path: str, model_pipeline_path: str, model_name: str):
@@ -56,9 +58,9 @@ def model_predict(test_data_path: str, model_pipeline_path: str, model_name: str
         metrics = {
             "mean_absolute_error": mae,
             "mean_squared_error": mse,
-            "r2_score": r2
+            "r2_score": r2,
         }
-        logger.info('Model evaluation metrics calculated')
+        logger.info("Model evaluation metrics calculated")
 
         return model, metrics
 
@@ -71,13 +73,13 @@ def model_predict(test_data_path: str, model_pipeline_path: str, model_name: str
         raise
 
 
-def save_model_info(run_id: str, model_path: str, file_path: str) -> None:
+def save_model_info(run_id: str, model_path: str, file_path: str, url: str) -> None:
     """Save the model run ID and path to a JSON file."""
     try:
-        model_info = {'run_id': run_id, 'model_path': model_path}
-        with open(file_path, 'w') as file:
-            json.dump(model_info, file, indent=4)
-        logger.info('Model info saved to %s', file_path)
+        model_info = {"run_id": run_id, "model_path": model_path, "url": url}
+        with open(file_path, "w") as file:
+            json.dump(model_info, file, indent=6)
+        logger.info("Model info saved to %s", file_path)
     except Exception:
         logger.exception("Error occurred while saving the model")
         raise
@@ -86,47 +88,48 @@ def save_model_info(run_id: str, model_path: str, file_path: str) -> None:
 def main():
     test_data_path = "./data/processed/test.csv"
     model_pipeline_path = "./models/model.pkl"
-    save_metrics_path = './reports/metrics.json'
+    save_metrics_path = "./reports/metrics.json"
     model_name = yaml_loader("./params.yaml")["model"]
+    experiment_name = yaml_loader("./params.yaml")["experiment_name"]
 
     try:
         # loading the model, calculating and saving the metrics using below function
-        model, metrics = model_predict(
-            test_data_path, model_pipeline_path, model_name)
+        model, metrics = model_predict(test_data_path, model_pipeline_path, model_name)
         save_metrics(metrics, save_metrics_path)
 
         # setting mlflow
-        os.environ["MLFLOW_TRACKING_USERNAME"] = repo_owner
-        os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
-        mlflow.set_tracking_uri(
-            f"{dagshub_url}/{repo_owner}/{repo_name}.mlflow")
-        logger.info(f"MLflow Tracking URI: {mlflow.get_tracking_uri()}")
-        mlflow.set_experiment("MLOPS_youtube_Project")
+        mlflow.set_experiment(experiment_name)
         with mlflow.start_run() as run:
 
             # logging the metrics to mlflow
             mlflow.log_metrics(metrics=metrics)
 
             # logging parameters
-            if hasattr(model, 'get_params'):
+            if hasattr(model, "get_params"):
                 params = model.get_params()
                 for param_name, param_value in params.items():
                     mlflow.log_param(param_name, param_value)
 
             # logging model to mlflow
-            mlflow.sklearn.log_model(model, name=f'youtube_model_{model_name}',
-                                     skops_trusted_types=["numpy.dtype", 'xgboost.core.Booster', 'xgboost.sklearn.XGBRegressor'])
+            mlflow.sklearn.log_model(
+                model,
+                name=f"youtube_model_{model_name}",
+                skops_trusted_types=["numpy.dtype"],
+            )
 
             # saving model information
             save_model_info(
-                run.info.run_id, f'youtube_model_{model_name}', 'reports/experiment_info.json')
+                run.info.run_id,
+                f"youtube_model_{model_name}",
+                "reports/experiment_info.json",
+                mlflow.get_tracking_uri(),
+            )
 
             # logging metrics file to mlflow
-            mlflow.log_artifact('reports/metrics.json')
+            mlflow.log_artifact("reports/metrics.json")
 
     except Exception:
-        logger.exception(
-            "Error occurred")
+        logger.exception("Error occurred")
         raise
 
 
